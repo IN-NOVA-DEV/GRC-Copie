14498f1e8ad22d0f4520b1ca54c168abe35ad91c
Rydanyaina <74042818+Rydanyainav8@users.noreply.github.com>
2023-12-12 15:21:42 +0300

    Fix Base Creation

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,8 +1,9 @@
-namespace GRC_Copie.Models;
-
-public class ErrorViewModel
+namespace GRC_Copie.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
