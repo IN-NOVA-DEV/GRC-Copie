c0381a4a7ae10a8fb18cc8cccb544aad586d4ef1
DylanRabe <129899278+DylanRabe@users.noreply.github.com>
2023-12-14 07:09:35 -0500

    Create Controller

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace grc_copie.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
