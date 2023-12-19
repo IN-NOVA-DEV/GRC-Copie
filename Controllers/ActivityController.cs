1ed7431c3f24803d540f7870f1fa8e5177d82544
Rydanyaina <74042818+Rydanyainav8@users.noreply.github.com>
2023-12-19 14:26:24 +0300

    Activity Features Display OK

---
+++ Controllers/ActivityController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace grc_copie.Controllers
+{
+    public class ActivityController : Controller
+    {
+        public IActionResult UserActivity()
+        {
+            return View();
+        }
+
+        public IActionResult AllActivity()
+        {
+            return View();
+        }
+    }
+}
